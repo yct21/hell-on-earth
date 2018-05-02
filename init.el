14580ef12f65c6b884e51d49ab97a6d8da4b1d6c
Chutian Yang <yct21@12tcy.com>
2018-05-02 10:11:17 +0800

    Setup proxy

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+(setq configuration-layer-elpa-archives
+        '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+          ("org-cn"   . "http://elpa.emacs-china.org/org/")
+          ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+
+(setq url-proxy-services
+    '(("http"     . "10.144.1.10:8080")
+        ("https"    . "10.144.1.10:8080")))
+
 (map-put default-frame-alist 'inhibit-double-buffering t)
 (toggle-frame-maximized)
 
