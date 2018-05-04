bc545d530e951f99b5210de265dc0c9e13d09e41
Chutian Yang <yct21@12tcy.com>
2018-05-04 14:45:54 +0800

    Stub setting for org-wiki

---
+++ modules/private/org/+org-wiki.el
@@ -0,0 +1,8 @@
+;;; private/org/+org-wiki.el -*- lexical-binding: t; -*-
+
+(def-package! org-wiki
+  :mode
+  :config
+  (setq org-wiki-location "~/mini-galaxy")
+  (setq org-wiki-server-port "96821"))
+
