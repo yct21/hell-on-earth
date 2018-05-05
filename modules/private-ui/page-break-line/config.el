ffd4fc2dcaf562eae1d3f5ccfc65a278244601b1
Chutian Yang <yct21@12tcy.com>
2018-05-06 02:40:44 +0800

    Disable page break line

--- modules/private-ui/page-break-line/config.el
+++ modules/private-ui/page-break-line/config.el
@@ -1,5 +1,5 @@
 ;;; private-ui/page-break-line/config.el -*- lexical-binding: t; -*-
 
-(def-package! page-break-lines
-  :init
-  (global-page-break-lines-mode 1))
+;; (def-package! page-break-lines
+;;   :init
+;;   (global-page-break-lines-mode 1))
