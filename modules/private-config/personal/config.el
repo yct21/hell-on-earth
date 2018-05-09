d02d3491ef177785d870616f161bd6155d1adac6
Chutian Yang <yct21@12tcy.com>
2018-05-09 14:10:51 +0800

    Change the width of json indent width

--- modules/private-config/personal/config.el
+++ modules/private-config/personal/config.el
@@ -9,3 +9,14 @@
 (setq-default frame-title-format
               '("欢迎回来，指挥官   |   "
                 (:eval (+workspace--tabline))))
+
+(setq-default evil-shift-width 2)
+
+(after! typescript-mode
+  (setq typescript-indent-level 2))
+
+(after! json-reformat
+  (setq json-reformat:indent-width 2))
+
+(after! js2-mode
+  (setq js2-basic-offset 2))
