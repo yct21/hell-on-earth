312a9381f149cf0ff821e24c4029187e90e84d98
Chutian Yang <yct21@12tcy.com>
2018-05-11 13:53:34 +0800

    Limit perps in title line

--- modules/private-config/personal/config.el
+++ modules/private-config/personal/config.el
@@ -6,9 +6,24 @@
 (after! avy
   (setq avy-all-windows 'all-frames))
 
+(defun +workspace-list-names-limited ()
+  (let* ((names (+workspace-list-names))
+         (substracted-names (seq-subseq names 0 (min 5 (length names))))
+        (current-name (+workspace-current-name)))
+    (mapconcat
+     #'identity
+     (cl-loop for i to (length substracted-names)
+              for name in substracted-names
+              collect
+              (propertize (format " [%d] %s " (1+ i) name)
+                          'face (if (equal current-name name)
+                                    '+workspace-tab-selected-face
+                                  '+workspace-tab-face)))
+     " ")))
+
 (setq-default frame-title-format
               '("欢迎回来，指挥官   |   "
-                (:eval (+workspace--tabline))))
+                (:eval (+workspace-list-names-limited))))
 
 (setq-default evil-shift-width 2)
 
