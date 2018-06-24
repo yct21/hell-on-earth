d8063b57ddcea9bc4dc677dba6cd9bc08fca406c
Chutian Yang <yct21@12tcy.com>
2018-06-24 08:44:56 +0800

    Update doom

--- config.el
+++ config.el
@@ -13,6 +13,54 @@
 
 (add-hook! minibuffer-setup (setq-local show-trailing-whitespace nil))
 
+(setq standard-indent 2)
+(setq-default evil-shift-width 2)
+
+(setq configuration-layer-elpa-archives
+        '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+          ("org-cn"   . "http://elpa.emacs-china.org/org/")
+          ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+
+(setq url-proxy-services
+   '(("http"     . "10.144.1.10:8080")
+       ("https"    . "10.144.1.10:8080")))
+
+(modify-frame-parameters nil '((inhibit-double-buffering . t)))
+(toggle-frame-maximized)
+
+;; I've swapped these keys on my keyboard
+(setq x-super-keysym 'alt
+      x-alt-keysym   'meta
+
+      user-mail-address "yct21@12tcy.com"
+      user-full-name    "Chutian Yang"
+
+      +org-dir (expand-file-name "~/orlog/")
+      org-ellipsis " ▼ "
+
+      ;; doom-variable-pitch-font (font-spec :family "Fira Sans")
+      ;; doom-unicode-font (font-spec :family "Input Mono Narrow" :size 12)
+      doom-big-font (font-spec :family "Fira Mono" :size 19))
+
+;; (pcase (system-name)
+;;   ((or "proteus" "halimede")
+;;    (setq ivy-height 12
+;;          +doom-modeline-height 24
+;;          ivy-posframe-font (font-spec :family "Input Mono Narrow" :size 12)
+;;          doom-font (font-spec :family "Input Mono Narrow" :size 10)))
+;;   (_
+;;    (setq ivy-posframe-font (font-spec :family "Input Mono Narrow" :size 18)
+;;          doom-font (font-spec :family "Input Mono Narrow" :size 16 :weight 'semi-light))))
+
+(pcase (system-name)
+  ((or "proteus" "halimede")
+   (setq ivy-posframe-font (font-spec :family "Source Code Pro" :size 16)
+         ivy-height 12))
+  (_
+   (setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'semi-light))))
+
+;;
+
 
 ;;
 ;; Keybindings
