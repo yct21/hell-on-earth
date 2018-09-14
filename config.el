0f230e62c34f54752268d566726ea3c19883f852
Chutian Yang <yct21@12tcy.com>
2018-09-14 18:48:11 +0800

    Put configurations in seperate module

--- config.el
+++ config.el
@@ -1,78 +1,5 @@
 ;;; private/hlissner/config.el -*- lexical-binding: t; -*-
 
-(defvar xdg-data (getenv "XDG_DATA_HOME"))
-;; (defvar xdg-bin (getenv "XDG_BIN_HOME"))
-;; (defvar xdg-cache (getenv "XDG_CACHE_HOME"))
-;; (defvar xdg-config (getenv "XDG_CONFIG_HOME"))
-
-(setq +doom-modeline-buffer-file-name-style 'relative-from-project
-      show-trailing-whitespace t
-
-      mu4e-maildir        (expand-file-name "mail" xdg-data)
-      mu4e-attachment-dir (expand-file-name "attachments" mu4e-maildir))
-
-(add-hook! minibuffer-setup (setq-local show-trailing-whitespace nil))
-
-(setq standard-indent 2)
-(setq-default evil-shift-width 2)
-
-(setq configuration-layer-elpa-archives
-        '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
-          ("org-cn"   . "http://elpa.emacs-china.org/org/")
-          ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
-
-(setq url-proxy-services
-   '(("http"     . "10.144.1.10:8080")
-       ("https"    . "10.144.1.10:8080")))
-
-(modify-frame-parameters nil '((inhibit-double-buffering . t)))
-(toggle-frame-maximized)
-
-;; I've swapped these keys on my keyboard
-(setq x-super-keysym 'alt
-      x-alt-keysym   'meta
-
-      user-mail-address "yct21@12tcy.com"
-      user-full-name    "Chutian Yang"
-
-      +org-dir (expand-file-name "~/orlog/")
-      org-ellipsis " ▼ "
-
-      ;; doom-variable-pitch-font (font-spec :family "Fira Sans")
-      ;; doom-unicode-font (font-spec :family "Input Mono Narrow" :size 12)
-      doom-big-font (font-spec :family "Fira Mono" :size 19))
-
-;; (pcase (system-name)
-;;   ((or "proteus" "halimede")
-;;    (setq ivy-height 12
-;;          +doom-modeline-height 24
-;;          ivy-posframe-font (font-spec :family "Input Mono Narrow" :size 12)
-;;          doom-font (font-spec :family "Input Mono Narrow" :size 10)))
-;;   (_
-;;    (setq ivy-posframe-font (font-spec :family "Input Mono Narrow" :size 18)
-;;          doom-font (font-spec :family "Input Mono Narrow" :size 16 :weight 'semi-light))))
-
-(pcase (system-name)
-  ((or "proteus" "halimede")
-   (setq ivy-posframe-font (font-spec :family "Source Code Pro" :size 16)
-         ivy-height 12))
-  (_
-   (setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'semi-light))))
-
-;;
-
-
-;;
-;; Keybindings
-;;
-
-(map!
- (:leader
-   (:prefix "f"
-     :desc "Find file in dotfiles" :n "t" #'+hlissner/find-in-dotfiles
-     :desc "Browse dotfiles"       :n "T" #'+hlissner/browse-dotfiles)))
-
-
 ;;
 ;; Modules
 ;;
@@ -93,7 +20,7 @@
 ;; tools/magit
 (after! magit
   (setq magit-repository-directories
-        (cl-loop for dir in (directory-files "~/work" t "^[^.]" t)
+        (cl-loop for dir in (directory-files "~/code" t "^[^.]" t)
                  if (file-directory-p dir)
                  nconc (cl-loop for subdir in (directory-files dir t "^[^.]" t)
                                 if (and (file-directory-p subdir)
@@ -105,3 +32,8 @@
 ;; The standard unicode characters are usually misaligned depending on the font.
 ;; This bugs me. Personally, markdown #-marks for headlines are more elegant.
 
+;;; load private settings
+
+(after! habitica
+  (setq habitica-uid "c81ddf50-0b40-4340-be82-189d6ee7d405")
+  (setq habitica-token "119c528f-c0c9-4820-8599-5d333ef06e22"))
--- modules/private-config/personal/config.el
+++ modules/private-config/personal/config.el
@@ -39,3 +39,62 @@
 
 (after! js2-mode
   (setq js2-basic-offset 2))
+
+;; I've swapped these keys on my keyboard
+(setq x-super-keysym 'alt
+      x-alt-keysym   'meta
+
+      user-mail-address "yct21@12tcy.com"
+      user-full-name    "Chutian Yang"
+
+      +org-dir (expand-file-name "~/orlog/")
+      org-ellipsis " ▼ "
+
+      ;; doom-variable-pitch-font (font-spec :family "Fira Sans")
+      ;; doom-unicode-font (font-spec :family "Input Mono Narrow" :size 12)
+      doom-big-font (font-spec :family "Fira Mono" :size 19))
+
+;; (pcase (system-name)
+;;   ((or "proteus" "halimede")
+;;    (setq ivy-height 12
+;;          +doom-modeline-height 24
+;;          ivy-posframe-font (font-spec :family "Input Mono Narrow" :size 12)
+;;          doom-font (font-spec :family "Input Mono Narrow" :size 10)))
+;;   (_
+;;    (setq ivy-posframe-font (font-spec :family "Input Mono Narrow" :size 18)
+;;          doom-font (font-spec :family "Input Mono Narrow" :size 16 :weight 'semi-light))))
+
+(pcase (system-name)
+  ((or "proteus" "halimede")
+   (setq ivy-posframe-font (font-spec :family "Source Code Pro" :size 16)
+         ivy-height 12))
+  (_
+   (setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'semi-light))))
+
+(modify-frame-parameters nil '((inhibit-double-buffering . t)))
+(toggle-frame-maximized)
+
+(defvar xdg-data (getenv "XDG_DATA_HOME"))
+;; (defvar xdg-bin (getenv "XDG_BIN_HOME"))
+;; (defvar xdg-cache (getenv "XDG_CACHE_HOME"))
+;; (defvar xdg-config (getenv "XDG_CONFIG_HOME"))
+
+(setq +doom-modeline-buffer-file-name-style 'relative-from-project
+      show-trailing-whitespace t
+
+      mu4e-maildir        (expand-file-name "mail" xdg-data)
+      mu4e-attachment-dir (expand-file-name "attachments" mu4e-maildir))
+
+(add-hook! minibuffer-setup (setq-local show-trailing-whitespace nil))
+
+(setq standard-indent 2)
+(setq-default evil-shift-width 2)
+
+(setq configuration-layer-elpa-archives
+        '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+          ("org-cn"   . "http://elpa.emacs-china.org/org/")
+          ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+
+(setq url-proxy-services
+   '(("http"     . "10.144.1.10:8080")
+       ("https"    . "10.144.1.10:8080")))
