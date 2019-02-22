90c8b820a80141b0a54cf0b96b888404cce0342b
Chutian Yang <yct21@12tcy.com>
2019-02-22 16:31:02 +0800

    Configure org-journal

--- modules/private-lang/org/+org-journal.el
+++ modules/private-lang/org/+org-journal.el
@@ -5,5 +5,18 @@
   :init
   (map! :leader :n "oj" #'org-journal-new-entry)
   :config
-  (setq org-journal-dir "~/orlog/stf")
+  (setq org-journal-dir "~/.stf")
+  (setq org-journal-file-format "%Y%m%d.stf")
+  (setq org-journal-find-file (lambda (filename)
+                                (let ((buffer (find-file-noselect filename)))
+                                  (display-buffer buffer)
+                                  )
+                                ))
   (setq org-journal-carryover-items nil))
+
+(set-popup-rule!
+  ".*\\.stf$"
+  :select t
+  :modeline t
+  :autosave t
+  )
