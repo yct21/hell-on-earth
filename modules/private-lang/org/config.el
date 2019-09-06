c410eb0856ceba6da3ae2522fccb59158ab2183d
Chutian Yang <yct21@12tcy.com>
2019-09-06 11:45:46 +0800

    Change obsolete function

--- modules/private-lang/org/config.el
+++ modules/private-lang/org/config.el
@@ -1,7 +1,7 @@
 ;;; private/org/config.el -*- lexical-binding: t; -*-
 
 (when (featurep! +org-projectile)
-  (def-package! org-projectile
+  (use-package! org-projectile
     :config
     (org-projectile-per-project)
     (setq org-projectile-per-project-filepath "TODOs.org"))
