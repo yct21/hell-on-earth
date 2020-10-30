feb7c8608aa44d96b9502b8f19833929f6b06ef2
Chutian Yang <yct21@12tcy.com>
2020-10-30 10:57:02 +0800

    Fix sis

--- modules/private-config/chinese/config.el
+++ modules/private-config/chinese/config.el
@@ -12,12 +12,25 @@
     (ace-pinyin-global-mode t)))
 
 (use-package! sis
+  :hook
+   (((text-mode prog-mode) . sis-follow-context-mode)
+   ((text-mode prog-mode) . sis-inline-mode))
   :config
-  ;; (add-hook 'text-mode-hook #'smart-input-source-mode)
-  (sis-global-respect-mode)
-  :init
-  (sis-ism-lazyman-config
+(sis-ism-lazyman-config
    "com.apple.keylayout.US"
    "com.logcg.inputmethod.LogInputMac2.LogInputMac2SP")
   ;; (setq smart-input-source-other-input-source "com.logcg.inputmethod.LogInputMac2.LogInputMac2SP")
+
+  ;; (add-hook 'text-mode-hook #'smart-input-source-mode)
+  ;; enable the /cursor color/ mode
+  (sis-global-cursor-color-mode t)
+  ;; enable the /respect/ mode
+  (sis-global-respect-mode t)
+  (setq
+    sis-inline-tighten-head-rule 0
+    sis-inline-tighten-tail-rule 0)
+  ;; enable the /follow context/ mode for all buffers
+  ;; (sis-global-follow-context-mode t)
+  ;; enable the /inline english/ mode for all buffers
+  ;; (sis-global-inline-mode t)
   )
